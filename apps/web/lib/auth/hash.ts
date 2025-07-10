22ac0e45f6c0fa3bb85a20ae644f556d535a3a14
rew <rew@tencorvids.com>
2025-07-10 16:50:31 -0400

    hash func, cookie helpers

---
+++ apps/web/lib/auth/hash.ts
@@ -0,0 +1,16 @@
+import { hash, verify } from "@node-rs/argon2";
+
+export async function hashPassword(password: string): Promise<string> {
+  return await hash(password, {
+    memoryCost: 19456,
+    timeCost: 2,
+    parallelism: 1,
+  });
+}
+
+export async function verifyPasswordHash(
+  hash: string,
+  password: string,
+): Promise<boolean> {
+  return await verify(hash, password);
+}
