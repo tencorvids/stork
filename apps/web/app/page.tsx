10a7a21f6f340afab927c051200256c4b935ce6c
rew <rew@tencorvids.com>
2025-07-11 15:31:14 -0400

    conditional render on auth

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,16 +1,27 @@
 import { Button } from "@stork/ui";
+import { getCurrentSession } from "@/auth/cookie";
 
-export default function Page() {
+export default async function Page() {
+    const session = await getCurrentSession()
+
     return (
         <main className="w-full h-full flex flex-col items-center justify-center">
             <div className="flex flex-col gap-4">
-                <Button asChild>
-                    <a href="/signup">Sign Up</a>
-                </Button>
+                {session.session ? (
+                    <p>Welcome back, {session.user.email}!</p>
+                ) : (
+                    <>
+                        <p>Welcome to Stork!</p>
 
-                <Button asChild variant="outline">
-                    <a href="/login">Login</a>
-                </Button>
+                        <Button asChild>
+                            <a href="/signup">Sign Up</a>
+                        </Button>
+
+                        <Button asChild variant="outline">
+                            <a href="/login">Login</a>
+                        </Button>
+                    </>
+                )}
             </div>
         </main>
     );
