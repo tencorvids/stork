1bdde19d6c49d8aa03a7d6baf24f35e2c8e78748
rew <rew@tencorvids.com>
2025-07-15 10:00:43 -0400

    markup fix

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Page() {
     const session = await getCurrentSession()
 
     return (
-        <main className="w-full h-full flex flex-col items-center justify-center">
+        <main className="w-full h-full min-h-svh flex flex-col items-center justify-center">
             <div className="flex flex-col gap-4">
                 {session.session ? (
                     <p>Welcome back, {session.user.email}!</p>
